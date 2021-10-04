7027416316a4fab4fe6775c8f19a12857dbda0e7
Masala <masala@riseup.net>
2021-10-04 16:09:03 -0400

    refactor setup.go

--- setup.go
+++ setup.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"os"
 
@@ -9,6 +8,7 @@ import (
 	"github.com/katzenpost/katzenpost/catshadow"
 	catconfig "github.com/katzenpost/katzenpost/catshadow/config"
 	"github.com/katzenpost/katzenpost/client"
+	"github.com/katzenpost/katzenpost/core/crypto/ecdh"
 	"path/filepath"
 )
 
@@ -26,6 +26,7 @@ func setupCatShadow(catshadowCfg *catconfig.Config, passphrase []byte, result ch
 	// XXX: if the catshadowClient already exists, shut it down
 	// FIXME: figure out a better way to toggle connected/disconnected
 	// states and allow to retry attempts on a timeout or other failure.
+	var catshadowClient *catshadow.Client
 	var stateWorker *catshadow.StateWriter
 	var state *catshadow.State
 	cfg, err := catshadowCfg.ClientConfig()
@@ -69,62 +70,53 @@ func setupCatShadow(catshadowCfg *catconfig.Config, passphrase []byte, result ch
 		return
 	}
 
-	var catshadowClient *catshadow.Client
-	cfg, linkKey, err := client.NewEphemeralClientConfig(cfg)
+	// automatically create a statefile if one does not already exist
+	stateLogger := backendLog.GetLogger("catshadow_state")
+	if _, err := os.Stat(statefile); os.IsNotExist(err) {
+		stateWorker, err = catshadow.NewStateWriter(stateLogger, statefile, passphrase)
+	} else {
+		stateWorker, state, err = catshadow.LoadStateWriter(stateLogger, statefile, passphrase)
+	}
 	if err != nil {
 		result <- err
 		return
 	}
 
+	// NewEphemeralClientConfig requires network connectivity to fetch a
+	// pki.Document and select a provider.
+	// TODO: fixme so that clients cache a consensus or set of provider descriptors
+	// which can be used to populate the config.Account section, or otherwise
+	// allow a client to be started without having config.Account set...
+	var linkKey *ecdh.PrivateKey
+	cfg, linkKey, err = client.NewEphemeralClientConfig(cfg)
+	if err != nil  {
+		result <- err
+		return
+	}
+
+	// create a client
 	c, err := client.New(cfg)
 	if err != nil {
 		result <- err
 		return
 	}
 
-	// automatically create a statefile if one does not already exist
-	if _, err := os.Stat(statefile); os.IsNotExist(err) {
-		// Create statefile.
-		stateWorker, err = catshadow.NewStateWriter(c.GetLogger("catshadow_state"), statefile, passphrase)
-		if err != nil {
-			result <- err
-			c.Shutdown()
-			return
-		}
-		// Start the stateworker
-		stateWorker.Start()
+	// Start the stateworker
+	stateWorker.Start()
 
-		// create a catshadow client and message spool
-		fmt.Println("creating remote message receiver spool")
-		user := fmt.Sprintf("%x", linkKey.PublicKey().Bytes())
+	user := linkKey.PublicKey().String()
+	// if there is no existing remote spool, create one:
+	if state.SpoolReadDescriptor == nil {
+		// TODO: would be nice to be able to defer spool creation if offline...
 		catshadowClient, err = catshadow.NewClientAndRemoteSpool(backendLog, c, stateWorker, user, linkKey)
-		if err != nil {
-			result <- err
-			stateWorker.Halt()
-			c.Shutdown()
-			return
-		}
-		fmt.Println("catshadow client successfully created")
 	} else {
-		// Load previous state to setup our current client state.
-		stateWorker, state, err = catshadow.LoadStateWriter(c.GetLogger("catshadow_state"), statefile, passphrase)
-		if err != nil {
-			result <- err
-			return
-		}
-
-		// Start the stateworker
-		stateWorker.Start()
-
-		// Make a catshadow Client.
 		catshadowClient, err = catshadow.New(backendLog, c, stateWorker, state)
-		if err != nil {
-			stateWorker.Halt()
-			result <- err
-			return
-		}
 	}
-
-	// Start catshadow client.
+	if err != nil {
+		result <- err
+		c.Shutdown()
+		stateWorker.Halt()
+		return
+	}
 	result <- catshadowClient
 }
