fbee482c0dc972b06e97fc9cfb6f4444d408bb03
Masala <masala@riseup.net>
2021-11-05 13:59:58 -0400

    use a layout.List for contact settings page

--- editcontact.go
+++ editcontact.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"gioui.org/gesture"
+	"gioui.org/io/pointer"
 	"gioui.org/layout"
+	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"image"
 )
 
 // EditContactPage is the page for adding a new contact
@@ -11,11 +15,13 @@ type EditContactPage struct {
 	a        *App
 	nickname string
 	back     *widget.Clickable
-	avatar   *widget.Clickable
+	avatar   *gesture.Click
 	clear    *widget.Clickable
 	expiry   *widget.Clickable
 	rename   *widget.Clickable
 	remove   *widget.Clickable
+	settings *layout.List
+	widgets  []layout.Widget
 	//avatar // select an avatar image
 }
 
@@ -27,30 +33,26 @@ func (p *EditContactPage) Layout(gtx layout.Context) layout.Dimensions {
 	}
 
 	return bg.Layout(gtx, func(gtx C) D {
-		return layout.Flex{Axis: layout.Vertical, Alignment: layout.End}.Layout(gtx,
+		return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceEnd, Alignment: layout.Start}.Layout(gtx,
+			// topbar
 			layout.Rigid(func(gtx C) D {
-				return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween, Alignment: layout.Baseline}.Layout(gtx,
+				return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween, Alignment: layout.Middle}.Layout(gtx,
 					layout.Rigid(button(th, p.back, backIcon).Layout),
 					layout.Flexed(1, fill{th.Bg}.Layout),
 					layout.Rigid(material.H6(th, "Edit Contact").Layout),
 					layout.Flexed(1, fill{th.Bg}.Layout))
 			}),
+			// settings list
 			layout.Flexed(1, func(gtx C) D {
-				return layout.Center.Layout(gtx, material.Button(th, p.avatar, "Choose Avatar").Layout)
+				in := layout.Inset{Top: unit.Dp(8), Bottom: unit.Dp(8), Left: unit.Dp(12), Right: unit.Dp(12)}
+				return in.Layout(gtx, func(gtx C) D {
+					return p.settings.Layout(gtx, len(p.widgets), func(gtx C, i int) layout.Dimensions {
+						// Layout the widget in the list. can wrap and inset, etc, here...
+						return p.widgets[i](gtx)
+					})
+				})
 			}),
-			layout.Flexed(1, func(gtx C) D {
-				return layout.Center.Layout(gtx, material.Button(th, p.clear, "Clear History").Layout)
-			}),
-			//layout.Flexed(1, func(gtx C) D {
-			//	return layout.Center.Layout(gtx, material.Button(th, p.expiry, "Set message lifetime").Layout)
-			//}),
-			//
-			layout.Flexed(1, func(gtx C) D {
-				return layout.Center.Layout(gtx, material.Button(th, p.rename, "Rename Contact").Layout)
-			}),
-			layout.Flexed(1, func(gtx C) D {
-				return layout.Center.Layout(gtx, material.Button(th, p.remove, "Delete Contact").Layout)
-			}))
+		)
 	})
 }
 
@@ -71,8 +73,17 @@ func (p *EditContactPage) Event(gtx layout.Context) interface{} {
 	if p.back.Clicked() {
 		return BackEvent{}
 	}
-	if p.avatar.Clicked() {
-		return ChooseAvatar{nickname: p.nickname}
+	for _, e := range p.avatar.Events(gtx.Queue) {
+		if e.Type == gesture.TypeClick {
+			c := p.a.c.GetContacts()[p.nickname]
+			if c.IsPending {
+				p.a.c.DeleteBlob("avatar://" + p.nickname)
+				ct := Contactal{}
+				ct.Reset()
+				c.SharedSecret = []byte(ct.SharedSecret)
+				return RedrawEvent{}
+			}
+		}
 	}
 	if p.clear.Clicked() {
 		// TODO: confirmation dialog
@@ -98,9 +109,29 @@ func (p *EditContactPage) Start(stop <-chan struct{}) {
 
 func newEditContactPage(a *App, contact string) *EditContactPage {
 	p := &EditContactPage{a: a, nickname: contact, back: &widget.Clickable{},
-		avatar: &widget.Clickable{}, clear: &widget.Clickable{},
+		avatar: &gesture.Click{}, clear: &widget.Clickable{},
 		expiry: &widget.Clickable{}, rename: &widget.Clickable{},
-		remove: &widget.Clickable{},
+		remove:   &widget.Clickable{},
+		settings: &layout.List{Axis: layout.Vertical},
+	}
+	p.widgets = []layout.Widget{
+		func(gtx C) D {
+			dims := layout.Center.Layout(gtx, func(gtx C) D {
+				gtx.Constraints.Max.X = gtx.Constraints.Max.X / 4
+				return layoutAvatar(gtx, p.a.c.GetContacts()[p.nickname])
+			})
+			a := pointer.Rect(image.Rectangle{Max: dims.Size})
+			t := a.Push(gtx.Ops)
+			p.avatar.Add(gtx.Ops)
+			t.Pop()
+			return dims
+		},
+		layout.Spacer{Height: unit.Dp(8)}.Layout,
+		material.Button(th, p.clear, "Clear History").Layout,
+		layout.Spacer{Height: unit.Dp(8)}.Layout,
+		material.Button(th, p.rename, "Rename Contact").Layout,
+		layout.Spacer{Height: unit.Dp(8)}.Layout,
+		material.Button(th, p.remove, "Delete Contact").Layout,
 	}
 	return p
 }
